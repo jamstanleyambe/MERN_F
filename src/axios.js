963fa7333ae78592622f7b00e22511e0d31ba972
jamstanleyambe <jamstanleyambe@gmail.com>
2024-02-12 23:24:00 +0100

    new

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
+
 const instance = axios.create({
-    baseURL: "https://mern-live-back-5e9639042351.herokuapp.com/"
+    baseURL: "https://mern-live-back-5e9639042351.herokuapp.com"
 })
 export default instance
+
